d8ba9f217b02f7c952d1821994994be8297e2892
gokhanmuzaffer <gokhangunesoglu@gmail.com>
2024-05-23 06:30:36 +0300

    Updates about ui

--- auth-server-ui/src/app/my-first-comp/my-first-comp.component.ts
+++ auth-server-ui/src/app/my-first-comp/my-first-comp.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
+import {MessageDetailsComponent} from "../message-details/message-details.component";
 
 @Component({
   selector: 'app-my-first-comp',
   standalone: true,
   imports: [
     FormsModule,
-    NgIf
+    NgIf,
+    NgForOf,
+    MessageDetailsComponent
   ],
   templateUrl: './my-first-comp.component.html',
   styleUrl: './my-first-comp.component.scss'
@@ -17,11 +20,21 @@ export class MyFirstCompComponent {
   name: string = 'Gokhan';
   email: string = 'gmgunesoglu@mail.com';
   message: string = 'hello there';
-  isSubmit: boolean = false;
+  isSubmitted: boolean = false;
+  messages: Array<any> = []
 
   onClick(): void {
-    console.log("name: ", this.name, "\nemail: ", this.email, "\nmessage: ", this.message);
-    this.isSubmit = true;
+    // console.log("name: ", this.name, "\nemail: ", this.email, "\nmessage: ", this.message);
+    this.isSubmitted = true;
+    this.messages.push({
+      'name': this.name,
+      'email': this.email,
+      'message': this.message
+    });
+  }
+
+  deleteMessage(index: number):void {
+    this.messages.splice(index, 1)
   }
 }
 
